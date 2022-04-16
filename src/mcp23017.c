5576c8eb5fcd3ce5ddb08221ed9a7e23cb0f491d
hpfousac <hpfousac@gmail.com>
2022-04-16 09:23:41 +0200

    Another CPU types, avoid possible conflicts with predefined contants

--- src/mcp23017.c
+++ src/mcp23017.c
@@ -3,36 +3,13 @@
 
 #include "mcp23017.h"
 
-// Registers
-#define REGISTER_IODIRA		0x00
-#define REGISTER_IODIRB		0x01
-#define REGISTER_IPOLA		0x02
-#define REGISTER_IPOLB		0x03
-#define REGISTER_GPINTENA	0x04
-#define REGISTER_GPINTENB	0x05
-#define REGISTER_DEFVALA	0x06
-#define REGISTER_DEFVALB	0x07
-#define REGISTER_INTCONA	0x08
-#define REGISTER_INTCONB	0x09
-//	IOCON			0x0A
-//	IOCON			0x0B
-#define REGISTER_GPPUA		0x0C
-#define REGISTER_GPPUB		0x0D
-#define REGISTER_INTFA		0x0E
-#define REGISTER_INTFB		0x0F
-#define REGISTER_INTCAPA	0x10
-#define REGISTER_INTCAPB	0x11
-#define REGISTER_GPIOA		0x12
-#define REGISTER_GPIOB		0x13
-#define REGISTER_OLATA		0x14
-#define REGISTER_OLATB		0x15
-
 #define I2C_TIMEOUT		10
 
 void mcp23017_init(MCP23017_HandleTypeDef *hdev, I2C_HandleTypeDef *hi2c, uint16_t addr)
 {
 	hdev->hi2c = hi2c;
-	hdev->addr = addr << 1;
+	hdev->addr = addr << 1; // tested using logan
+	// hdev->addr = addr;
 }
 
 HAL_StatusTypeDef mcp23017_read(MCP23017_HandleTypeDef *hdev, uint16_t reg, uint8_t *data)
@@ -48,26 +25,26 @@ HAL_StatusTypeDef mcp23017_write(MCP23017_HandleTypeDef *hdev, uint16_t reg, uin
 HAL_StatusTypeDef mcp23017_iodir(MCP23017_HandleTypeDef *hdev, uint8_t port, uint8_t iodir)
 {
 	uint8_t data[1] = {iodir};
-	return mcp23017_write(hdev, REGISTER_IODIRA|port, data);
+	return mcp23017_write(hdev, MCP23017_REGISTER_IODIRA|port, data);
 }
 
 HAL_StatusTypeDef mcp23017_ipol(MCP23017_HandleTypeDef *hdev, uint8_t port, uint8_t ipol)
 {
 	uint8_t data[1] = {ipol};
-	return mcp23017_write(hdev, REGISTER_IPOLA|port, data);
+	return mcp23017_write(hdev, MCP23017_REGISTER_IPOLA|port, data);
 }
 
 HAL_StatusTypeDef mcp23017_ggpu(MCP23017_HandleTypeDef *hdev, uint8_t port, uint8_t pu)
 {
 	uint8_t data[1] = {pu};
-	return mcp23017_write(hdev, REGISTER_GPPUA|port, data);
+	return mcp23017_write(hdev, MCP23017_REGISTER_GPPUA|port, data);
 }
 
 HAL_StatusTypeDef mcp23017_read_gpio(MCP23017_HandleTypeDef *hdev, uint8_t port)
 {
 	uint8_t data[1];
 	HAL_StatusTypeDef status;
-	status = mcp23017_read(hdev, REGISTER_GPIOA|port, data);
+	status = mcp23017_read(hdev, MCP23017_REGISTER_GPIOA|port, data);
 	if (status == HAL_OK)
 		hdev->gpio[port] = data[0];
 	return status;
@@ -76,5 +53,5 @@ HAL_StatusTypeDef mcp23017_read_gpio(MCP23017_HandleTypeDef *hdev, uint8_t port)
 HAL_StatusTypeDef mcp23017_write_gpio(MCP23017_HandleTypeDef *hdev, uint8_t port)
 {
 	uint8_t data[1] = {hdev->gpio[port]};
-	return mcp23017_write(hdev, REGISTER_GPIOA|port, data);
+	return mcp23017_write(hdev, MCP23017_REGISTER_GPIOA|port, data);
 }
--- src/mcp23017.h
+++ src/mcp23017.h
@@ -1,11 +1,51 @@
 // Copyright (c) 2018 Rudá Moura <ruda.moura@gmail.com>
 // License: The MIT License (MIT)
 
-#include "stm32f1xx_hal.h"
+#if defined(STM32F103xB)
+# include "stm32f1xx_hal.h"
+#endif
+
+#if defined(STM32F401xC)
+# include "stm32f4xx_hal.h"
+#endif
+
+#if defined(STM32F407xx)
+# include "stm32f4xx_hal.h"
+#endif
+
+#if defined(STM32H750xx)
+# include "stm32h7xx_hal.h"
+#endif
 
 // Ports
 #define MCP23017_PORTA			0x00
 #define MCP23017_PORTB			0x01
+
+// Registers
+#define MCP23017_REGISTER_IODIRA		0x00
+#define MCP23017_REGISTER_IODIRB		0x01
+#define MCP23017_REGISTER_IPOLA		0x02
+#define MCP23017_REGISTER_IPOLB		0x03
+#define MCP23017_REGISTER_GPINTENA	0x04
+#define MCP23017_REGISTER_GPINTENB	0x05
+#define MCP23017_REGISTER_DEFVALA	0x06
+#define MCP23017_REGISTER_DEFVALB	0x07
+#define MCP23017_REGISTER_INTCONA	0x08
+#define MCP23017_REGISTER_INTCONB	0x09
+#define MCP23017_REGISTER_IOCON      0x0A
+
+//	IOCON			0x0A
+//	IOCON			0x0B
+#define MCP23017_REGISTER_GPPUA		0x0C
+#define MCP23017_REGISTER_GPPUB		0x0D
+#define MCP23017_REGISTER_INTFA		0x0E
+#define MCP23017_REGISTER_INTFB		0x0F
+#define MCP23017_REGISTER_INTCAPA	0x10
+#define MCP23017_REGISTER_INTCAPB	0x11
+#define MCP23017_REGISTER_GPIOA		0x12
+#define MCP23017_REGISTER_GPIOB		0x13
+#define MCP23017_REGISTER_OLATA		0x14
+#define MCP23017_REGISTER_OLATB		0x15
 
 // Address (A0-A2)
 #define MCP23017_ADDRESS_20		0x20
@@ -68,5 +108,5 @@ HAL_StatusTypeDef	mcp23017_write(MCP23017_HandleTypeDef *hdev, uint16_t reg, uin
 HAL_StatusTypeDef	mcp23017_iodir(MCP23017_HandleTypeDef *hdev, uint8_t port, uint8_t iodir);
 HAL_StatusTypeDef	mcp23017_ipol(MCP23017_HandleTypeDef *hdev, uint8_t port, uint8_t ipol);
 HAL_StatusTypeDef	mcp23017_ggpu(MCP23017_HandleTypeDef *hdev, uint8_t port, uint8_t pu);
-HAL_StatusTypeDef	mcp23017_read_gpio(MCP23017_HandleTypeDef *hdev, uint8_t port);
+HAL_StatusTypeDef	mcp23017_read_gpio(MCP23017_HandleTypeDef *hdev, uint8_t port); // port index [0,1] or MCP23017_PORTA/MCP23017_PORTB
 HAL_StatusTypeDef	mcp23017_write_gpio(MCP23017_HandleTypeDef *hdev, uint8_t port);
